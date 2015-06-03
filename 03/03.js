d9cdc809e70c7fada668dfa15e9a88d3f15a2ee6
Michael Wiss <michael.wiss@gmail.com>
2015-06-03 15:21:29 -0500

    comparing event target with selector

--- 03/03.js
+++ 03/03.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
-   $('#switcher-default').addClass('selected')
-   $('#switcher button') .click(function() {
-   	   var bodyClass = this.id.split('-') [1];
-   	   $('body').removeClass().addClass(bodyClass);
-   	   $('#switcher button').removeClass('selected');
-   	   $(this).addClass('selected');
-   });
-   //$('#switcher-narrow').on('click', function() {
-   // 	   $('body').addClass('narrow');
-   // });
-   // $('#switcher-large').on('click', function() {
-   //     $('body').addClass('large');
-	  // });
-});
+// $(document).ready(function() {
+//    $('#switcher-default').addClass('selected')
+//    $('#switcher button') .click(function() {
+//    	   var bodyClass = this.id.split('-') [1];
+//    	   $('body').removeClass().addClass(bodyClass);
+//    	   $('#switcher button').removeClass('selected');
+//    	   $(this).addClass('selected');
+//    });
+//    //$('#switcher-narrow').on('click', function() {
+//    // 	   $('body').addClass('narrow');
+//    // });
+//    // $('#switcher-large').on('click', function() {
+//    //     $('body').addClass('large');
+// 	  // });
+// });
 
 // $(document).ready(function() {
 // 	$('#switcher').hover(function() {
@@ -23,10 +23,32 @@ $(document).ready(function() {
 // });
 
 
+// $(document).ready(function() {
+// 	$('#switcher').click(function(event) {
+// 	if (event.target == this) {
+// 		$('#switcher button').toggleClass('hidden');
+// 	}
+// 	});
+// });
+// $(document).ready(function() {
+//    $('#switcher-default').addClass('selected')
+//    $('#switcher button') .click(function(event) {
+//    	   var bodyClass = this.id.split('-') [1];
+//    	   $('body').removeClass().addClass(bodyClass);
+//    	   $('#switcher button').removeClass('selected');
+//    	   $(this).addClass('selected');
+//    	   event.stopPropagation();
+//    });
+//  });
+
 $(document).ready(function() {
-	$('#switcher').click(function(event) {
-	if (event.target == this) {
-		$('#switcher button').toggleClass('hidden');
-	}
-	});
+   $('#switcher').click(function(event) {
+      if ($(event.target).is('button')) {
+         var bodyClass = event.target.id.split('-') [1];
+         $('body').removeClass().addClass(bodyClass);
+         $('#switcher button').removeClass('selected');
+         $(event.target).addClass('selected');
+         event.stopPropagation();
+      }
+   });
 });
