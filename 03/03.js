43919edbc623de50e822a9ddf89023f467c575ad
Michael Wiss <michael.wiss@gmail.com>
2015-06-03 15:37:20 -0500

    using is to test if switcher is a button

--- 03/03.js
+++ 03/03.js
@@ -41,6 +41,33 @@
 //    });
 //  });
 
+// $(document).ready(function() {
+//    $('#switcher').click(function(event) {
+//       if ($(event.target).is('button')) {
+//          var bodyClass = event.target.id.split('-') [1];
+//          $('body').removeClass().addClass(bodyClass);
+//          $('#switcher button').removeClass('selected');
+//          $(event.target).addClass('selected');
+//          event.stopPropagation();
+//       }
+//    });
+// });
+
+$(document).ready(function() {
+   $('#switcher').hover(function() {
+      $(this).addClass('hover');
+   }, function() {
+      $(this).removeClass('hover');
+   });
+});
+$(document).ready(function() {
+   $('#switcher').click(function(event) {
+      if (!$(event.target).is('button')) {
+         $('#switcher button').toggleClass('hidden');
+      }
+   });
+});
+
 $(document).ready(function() {
    $('#switcher').click(function(event) {
       if ($(event.target).is('button')) {
@@ -52,3 +79,13 @@ $(document).ready(function() {
       }
    });
 });
+
+
+
+
+
+
+
+
+
+
